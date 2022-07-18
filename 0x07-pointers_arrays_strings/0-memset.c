9e72c94f0e4dc0c08ac3afb103dca7c46284ee5b
Gizachew Ayalew <ewayegiz@gmail.com>
2022-07-18 12:26:14 -0700

    pointer and array

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,40 +1,20 @@
+
 #include "main.h"
-#include <stdio.h>
 /**
-* simple_print_buffer - prints buffer in hexa
-* @buffer: the address of memory to print
-* @size: the size of the memory to print
-* Return: Nothing.
+*_memset - fill memory with constant
+*@s: space of memory
+*@b: byte
+*@n: memory area
+*Return: pointer to the memory area
 */
-void simple_print_buffer(char *buffer, unsigned int size)
+char *_memset(char *s, char b, unsigned int n)
 {
 unsigned int i;
 i = 0;
-while (i < size)
+while (i < n)
 {
-if (i % 10)
-{
-printf(" ");
-}
-if (!(i % 10) && i)
-{
-printf("\n");
-}
-printf("0x%02x", buffer[i]);
+*(s + i) = b;
 i++;
 }
-printf("\n");
-}
-/**
-* main - check the code for Holberton School students.
-* Return: Always 0.
-*/
-int main(void)
-{
-char buffer[98] = {0x00};
-simple_print_buffer(buffer, 98);
-_memset(buffer, 0x01, 95);
-printf("-------------------------------------------------\n");
-simple_print_buffer(buffer, 98);
-return (0);
+return (s);
 }
